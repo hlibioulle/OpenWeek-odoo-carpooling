ccc6e5b5f39588edbb9b8e7ad6d4e3d72bfd49c7
hlibioulle <hadrien.libioulle@student.uclouvain.be>
2022-06-29 10:54:38 +0200

    first model for carpooling system and open academy progress

--- carpooling/models/models.py
+++ carpooling/models/models.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
+class SharedVehicle(models.Model):
+    _name = 'carpooling.shared_car'
+    _description = 'carpooling.shared_car.description'
 
-class carpooling(models.Model):
-     _name = 'carpooling.carpooling'
-     _description = 'carpooling.carpooling'
+    driver = fields.Many2one("res.users", "Driver", required=True)
+    vehicle_type = fields.Char()
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    departure_loc = fields.Char(required=True)
+    destination_loc = fields.Char(required=True)
+    departure_time = fields.Datetime(required=True)
 
-     @api.depends('value')
-     def _value_pc(self):
-         for record in self:
-             record.value2 = float(record.value) / 100
+    avalaible_seats = fields.Integer(required=True)
+    
+    description = fields.Text()
+
+    passengers = fields.Many2many(comodel_name="carpooling.passenger", string="Passenger")
+
+class Passenger(models.Model):
+    _name = "carpooling.passenger"
+    _description = "carpooling.passenger.description"
+    _inherit = "res.users"
+
+    shared_car = fields.Many2many("res.users", "Passenger")
